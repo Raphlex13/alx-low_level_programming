ec1ff41c151887bd21603f536724074f4b2d93c8
Raphael <raphaelugenlo@gmail.com>
2022-09-15 17:45:46 -0700

    Task done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print  0 - 9
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar(10);
+}
